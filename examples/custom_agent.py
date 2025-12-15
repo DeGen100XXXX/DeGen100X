850d3f24160aef6d771e942f65a192551056abe3
HASHBOI <eduardermakov769@gmail.com>
2025-12-15 22:09:03 +0800

    Update custom_agent.py

--- examples/custom_agent.py
+++ examples/custom_agent.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('agent_log.log'),
+        logging.FileHandler('agent_log.log'), 
         logging.StreamHandler()
     ]
 )
